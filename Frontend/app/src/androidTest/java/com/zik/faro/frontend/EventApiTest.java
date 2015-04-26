38fcb15ac423085dae0f024fc056c57ec61ad975
Nakul Shah <nakulshah87@gmail.com>
2015-04-26 14:07:55 -0700

    Included http code in frontend

---
+++ Frontend/app/src/androidTest/java/com/zik/faro/frontend/EventApiTest.java
@@ -0,0 +1,144 @@
+package com.zik.faro.frontend;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+
+import com.squareup.okhttp.Request;
+import com.zik.faro.frontend.data.DateOffset;
+import com.zik.faro.frontend.data.EventCreateData;
+import com.zik.faro.frontend.data.MinEvent;
+import com.zik.faro.frontend.faroservice.FaroServiceHandler;
+import com.zik.faro.frontend.faroservice.HttpError;
+import com.zik.faro.frontend.faroservice.Callbacks.BaseFaroRequestCallback;
+import com.zik.faro.frontend.faroservice.okHttp.OKHttpWrapperEvent;
+
+import junit.framework.Assert;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class EventApiTest extends ApplicationTestCase<Application> {
+    public EventApiTest() {
+        super(Application.class);
+    }
+    private static final String baseUrl = "http://10.0.2.2:8080/v1/";
+
+
+    @LargeTest
+    public void testGetEvent() throws InterruptedException, MalformedURLException {
+        final Semaphore waitSem = new Semaphore(0);
+        FaroServiceHandler serviceHandler = FaroServiceHandler.getFaroServiceHandler(new URL(baseUrl));
+
+
+        TestGetEventCallbackHandler callback = new TestGetEventCallbackHandler(waitSem, 404);
+        serviceHandler.getEventHandler().getEvent(callback, "randomEventId");
+        boolean timeout = false;
+        timeout = !waitSem.tryAcquire(3000, TimeUnit.MILLISECONDS);
+
+        Assert.assertFalse(timeout);
+        Assert.assertFalse(callback.failed);
+        Assert.assertFalse(callback.unexpectedResponseCode);
+    }
+
+    @LargeTest
+    public void testCreateGetEvent() throws InterruptedException, MalformedURLException {
+        final Semaphore waitSem = new Semaphore(0);
+        FaroServiceHandler serviceHandler = FaroServiceHandler.getFaroServiceHandler(new URL(baseUrl));
+        TestEventCreateCallback createCallback = new TestEventCreateCallback(waitSem, 200, null);
+
+        EventCreateData eventCreateData= new EventCreateData("MySampleEvent", new DateOffset(new Date(), 1000), null, null, null);
+
+        serviceHandler.getEventHandler().createEvent(createCallback, eventCreateData);
+        boolean timeout;
+        timeout = !waitSem.tryAcquire(3000, TimeUnit.MILLISECONDS);
+
+        Assert.assertFalse(timeout);
+        Assert.assertFalse(createCallback.failed);
+        Assert.assertFalse(createCallback.unexpectedResponseCode);
+
+        String eventId = createCallback.receivedMinEvent.id;
+        TestGetEventCallbackHandler callback = new TestGetEventCallbackHandler(waitSem, 200);
+        serviceHandler.getEventHandler().getEvent(callback, eventId);
+        timeout = !waitSem.tryAcquire(3000, TimeUnit.MILLISECONDS);
+
+        Assert.assertFalse(timeout);
+        // TODO: check why start date is null for received event
+        Assert.assertNotNull(callback.receivedEvent);
+        Assert.assertFalse(callback.failed);
+        Assert.assertFalse(callback.unexpectedResponseCode);
+    }
+
+    @LargeTest
+    public void testCreateEvent() throws InterruptedException, MalformedURLException {
+        final Semaphore waitSem = new Semaphore(0);
+        OKHttpWrapperEvent reqEvent = new OKHttpWrapperEvent(new URL(baseUrl));
+        TestEventCreateCallback callback = new TestEventCreateCallback(waitSem, 200, null);
+
+        EventCreateData eventCreateData= new EventCreateData("MySampleEvent", new DateOffset(new Date(), 1000), null, null, null);
+
+        reqEvent.createEvent(callback, eventCreateData);
+        boolean timeout = false;
+        timeout = !waitSem.tryAcquire(3000, TimeUnit.MILLISECONDS);
+
+        Assert.assertFalse(timeout);
+        Assert.assertFalse(callback.failed);
+        Assert.assertFalse(callback.unexpectedResponseCode);
+    }
+
+
+    final static class TestEventCreateCallback
+            extends Utils.BaseTestCallbackHandler
+            implements BaseFaroRequestCallback<MinEvent>{
+        MinEvent expectedMinEvent;
+        MinEvent receivedMinEvent;
+        TestEventCreateCallback(Semaphore semaphore, int expectedCode, MinEvent expectedEvent){
+            super(semaphore, expectedCode);
+            this.expectedMinEvent = expectedEvent;
+        }
+
+        @Override
+        public void onFailure(Request request, IOException e) {
+            waitSem.release();
+        }
+
+        @Override
+        public void onResponse(MinEvent event, HttpError error){
+            // Assert that event == min event
+            this.receivedMinEvent = event;
+            waitSem.release();
+        }
+    }
+
+    final static class TestGetEventCallbackHandler<Event>
+            extends Utils.BaseTestCallbackHandler implements BaseFaroRequestCallback<Event>{
+        public Event receivedEvent;
+        TestGetEventCallbackHandler(final Semaphore sem, int expectedCode){
+            super(sem, expectedCode);
+        }
+
+        @Override
+        public void onFailure(Request request, IOException e) {
+            Log.v("Get Event", "Failed");
+            failed = true;
+            waitSem.release();
+        }
+
+        @Override
+        public void onResponse(Event event, HttpError error){
+            if(event != null){
+                this.receivedEvent = event;
+            }
+            Log.v("Get Event", "Success");
+            waitSem.release();
+        }
+    }
+}
---
+++ Frontend/app/src/androidTest/java/com/zik/faro/frontend/ProfileApiTest.java
@@ -0,0 +1,113 @@
+package com.zik.faro.frontend;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+
+import com.squareup.okhttp.Request;
+import com.zik.faro.frontend.data.user.FaroUser;
+import com.zik.faro.frontend.faroservice.FaroServiceHandler;
+import com.zik.faro.frontend.faroservice.HttpError;
+import com.zik.faro.frontend.faroservice.Callbacks.BaseFaroRequestCallback;
+
+import junit.framework.Assert;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class ProfileApiTest extends ApplicationTestCase<Application> {
+    public ProfileApiTest() {
+        super(Application.class);
+    }
+    private static final String baseUrl = "http://10.0.2.2:8080/v1/";
+
+    @LargeTest
+    public void testCreateGetProfile() throws InterruptedException, MalformedURLException {
+        final Semaphore waitSem = new Semaphore(0);
+        FaroServiceHandler serviceHandler = FaroServiceHandler.getFaroServiceHandler(new URL(baseUrl));
+
+        String uuidFName = UUID.randomUUID().toString();
+        String uuidMName = UUID.randomUUID().toString();
+        String uuidLName = UUID.randomUUID().toString();
+        String uuidExpense = UUID.randomUUID().toString();
+        String uuidTelephone = UUID.randomUUID().toString();
+        String uuidEmail = UUID.randomUUID().toString() + "@gmail.com";
+        FaroUser faroUser = new FaroUser(uuidEmail, uuidFName, uuidMName, uuidLName, uuidExpense, uuidTelephone, null);
+
+        TestCreateProfileCallbackHandler callback = new TestCreateProfileCallbackHandler(waitSem, 200);
+        serviceHandler.getProfileHandler().createProfile(callback, faroUser);
+        boolean timeout = false;
+        timeout = !waitSem.tryAcquire(3000, TimeUnit.MILLISECONDS);
+
+        Assert.assertFalse(timeout);
+        Assert.assertFalse(callback.failed);
+
+        TestGetProfileCallbackHandler getCallback = new TestGetProfileCallbackHandler(waitSem, 200);
+        serviceHandler.getProfileHandler().getProfile(getCallback, uuidEmail);
+        timeout = !waitSem.tryAcquire(3000, TimeUnit.MILLISECONDS);
+        Assert.assertFalse(timeout);
+        Assert.assertFalse(getCallback.failed);
+        Assert.assertNotNull(getCallback.receivedFaroUser);
+        Assert.assertEquals(getCallback.receivedFaroUser.getFirstName(), uuidFName);
+        Assert.assertEquals(getCallback.receivedFaroUser.getLastName(), uuidLName);
+        Assert.assertEquals(getCallback.receivedFaroUser.getMiddleName(), uuidMName);
+        Assert.assertEquals(getCallback.receivedFaroUser.getExternalExpenseID(), uuidExpense);
+        Assert.assertEquals(getCallback.receivedFaroUser.getTelephone(), uuidTelephone);
+    }
+
+    final static class TestGetProfileCallbackHandler
+            extends Utils.BaseTestCallbackHandler implements BaseFaroRequestCallback<FaroUser> {
+        public FaroUser receivedFaroUser;
+        TestGetProfileCallbackHandler(final Semaphore sem, int expectedCode){
+            super(sem, expectedCode);
+        }
+
+        @Override
+        public void onFailure(Request request, IOException e) {
+            failed = true;
+            Log.v("Get Profile", "Failed");
+            waitSem.release();
+        }
+
+        @Override
+        public void onResponse(FaroUser response, HttpError error){
+            if(response!= null){
+                this.receivedFaroUser = response;
+            }else{
+                failed = true;
+            }
+            waitSem.release();
+        }
+    }
+
+    final static class TestCreateProfileCallbackHandler
+            extends Utils.BaseTestCallbackHandler implements BaseFaroRequestCallback<String> {
+        public String receivedEvent;
+        TestCreateProfileCallbackHandler(final Semaphore sem, int expectedCode){
+            super(sem, expectedCode);
+        }
+
+        @Override
+        public void onFailure(Request request, IOException e) {
+            Log.v("Create Profile", "Failed");
+            failed = true;
+            waitSem.release();
+        }
+
+        @Override
+        public void onResponse(String response, HttpError error){
+            if(response!= null){
+                this.receivedEvent = response;
+            }else{
+                failed = true;
+            }
+            Log.v("Create Profile", "Success");
+            waitSem.release();
+        }
+    }
+}
