fa73c8b279732a1b573a5d2284764d94f528eb1f
pgore <pgore@adobe.com>
2015-10-24 11:40:04 -0700

    Enabling cast and unvoted tests

--- Frontend/app/src/androidTest/java/com/zik/faro/frontend/PollApiTest.java
+++ Frontend/app/src/androidTest/java/com/zik/faro/frontend/PollApiTest.java
@@ -21,7 +21,9 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
@@ -81,8 +83,8 @@ public class PollApiTest extends ApiBaseTest{
         Assert.assertNotNull(pollCreateCallback.receivedPoll);
         Assert.assertFalse(pollCreateCallback.failed);
         Assert.assertFalse(pollCreateCallback.unexpectedResponseCode);
-        
         Poll createdPoll = pollCreateCallback.receivedPoll;
+        
         // Get Poll
         TestPollGetCallback getPollCallback = new TestPollGetCallback(waitSem, 200);
         serviceHandler.getPollHandler().getPoll(getPollCallback, eventId, createdPoll.getId());
@@ -95,19 +97,23 @@ public class PollApiTest extends ApiBaseTest{
         Assert.assertTrue(poll1.getCreatorId().equals(receivedPoll.getCreatorId()));
         Assert.assertTrue(receivedPoll.getStatus() == ObjectStatus.OPEN);
         
-        /*
+        
         // Cast Vote
         //TODO: finish implementation at the backend
         Set<String> options = new HashSet<>();
         options.add(optionValue);
-        TestPollCastVoteCallback castVoteCallback = new TestPollCastVoteCallback(waitSem, 200);
-        serviceHandler.getPollHandler().castVote(castVoteCallback, eventId, poll1.getId(), options);
+        TestOKActionCallbackHandler castVoteCallback = new TestOKActionCallbackHandler(waitSem, 200);
+        serviceHandler.getPollHandler().castVote(castVoteCallback, eventId, receivedPoll.getId(), options);
         timeout = !waitSem.tryAcquire(testTimeout, TimeUnit.MILLISECONDS);
         Assert.assertFalse(timeout);
         Assert.assertFalse(castVoteCallback.failed);
         Assert.assertFalse(castVoteCallback.unexpectedResponseCode);
         
+        TestPollGetCallback votedPoll = new TestPollGetCallback(waitSem, 200);
+        serviceHandler.getPollHandler().getPoll(votedPoll, eventId, receivedPoll.getId());
+        Poll p = votedPoll.receivedPoll;
         
+        /*        
         // Close Poll
         //TODO: finish implementation at the backend 
         TestPollCloseCallback pollCloseCallback = new TestPollCloseCallback(waitSem, 200);
@@ -120,18 +126,18 @@ public class PollApiTest extends ApiBaseTest{
 
         // Get unvoted count
         //TODO: finish implementation at the backend
-        /*
         TestPollUnvotedCountCallback unvotedCountCallback = new TestPollUnvotedCountCallback(waitSem, 200);
         serviceHandler.getPollHandler().getUnvotedCount(unvotedCountCallback, eventId);
         timeout = !waitSem.tryAcquire(testTimeout, TimeUnit.MILLISECONDS);
         Assert.assertFalse(timeout);
         Assert.assertFalse(unvotedCountCallback.failed);
         Assert.assertFalse(unvotedCountCallback.unexpectedResponseCode);
-        */
+        int i = unvotedCountCallback.unvotedCount;
+        /**/
         
         // Delete Poll
         TestOKActionCallbackHandler deletePollCallBack = new TestOKActionCallbackHandler(waitSem, 200);
-        serviceHandler.getPollHandler().deletePoll(deletePollCallBack, eventId, poll1.getId());
+        serviceHandler.getPollHandler().deletePoll(deletePollCallBack, eventId, receivedPoll.getId());
         timeout = !waitSem.tryAcquire(testTimeout, TimeUnit.MILLISECONDS);
         Assert.assertFalse(timeout);
         Assert.assertFalse(deletePollCallBack.failed);
@@ -139,7 +145,7 @@ public class PollApiTest extends ApiBaseTest{
         
         // Verify that poll has been deleted
         getPollCallback = new TestPollGetCallback(waitSem, 404);
-        serviceHandler.getPollHandler().getPoll(getPollCallback, eventId, poll1.getId());
+        serviceHandler.getPollHandler().getPoll(getPollCallback, eventId, receivedPoll.getId());
         timeout = !waitSem.tryAcquire(testTimeout, TimeUnit.MILLISECONDS);
         Assert.assertFalse(timeout);
         Assert.assertTrue(getPollCallback.failed);
@@ -178,9 +184,10 @@ public class PollApiTest extends ApiBaseTest{
     }
 
     static class TestPollUnvotedCountCallback extends Utils.BaseTestCallbackHandler implements BaseFaroRequestCallback<Integer> {
-        public Integer unvotedCount;
+        public int unvotedCount;
         TestPollUnvotedCountCallback(Semaphore semaphore, int expectedCode) {
             super(semaphore, expectedCode);
+            this.unvotedCount = 0;
         }
 
         @Override
@@ -194,7 +201,9 @@ public class PollApiTest extends ApiBaseTest{
             if(error != null){
                 this.failed = true;
             }
-            this.unvotedCount = integer;
+            else {
+                this.unvotedCount = integer.intValue();
+            }
             waitSem.release();
         }
     }
@@ -224,12 +233,6 @@ public class PollApiTest extends ApiBaseTest{
         }
     }
     
-    
-    static class TestPollCastVoteCallback extends TestPollCreateCallback{
-        TestPollCastVoteCallback(Semaphore semaphore, int expectedCode) {
-            super(semaphore, expectedCode);
-        }
-    }
 
     static class TestPollCloseCallback extends TestPollCreateCallback{
         TestPollCloseCallback(Semaphore semaphore, int expectedCode) {
