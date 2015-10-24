a03ac892e9d5f19dff821566fb1818c73c66de0c
pgore <pgore@adobe.com>
2015-10-24 12:25:25 -0700

    Adding Friends Handler

--- Frontend/app/src/main/java/com/zik/faro/frontend/faroservice/okHttp/OKHttpWrapperFriends.java
+++ Frontend/app/src/main/java/com/zik/faro/frontend/faroservice/okHttp/OKHttpWrapperFriends.java
@@ -1,4 +1,71 @@
 package com.zik.faro.frontend.faroservice.okHttp;
 
-public class OKHttpWrapperFriends {
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.zik.faro.frontend.data.MinUser;
+import com.zik.faro.frontend.data.Poll;
+import com.zik.faro.frontend.faroservice.Callbacks.BaseFaroRequestCallback;
+import com.zik.faro.frontend.faroservice.auth.TokenCache;
+import com.zik.faro.frontend.faroservice.spec.FriendsHandler;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OKHttpWrapperFriends extends BaseFaroOKHttpWrapper implements FriendsHandler{
+    OKHttpWrapperFriends(URL baseUrl, String pathPrefix) {
+        super(baseUrl, "friends");
+    }
+
+    @Override
+    public void inviteFriend(BaseFaroRequestCallback<String> callback, String userId) {
+        String token = TokenCache.getTokenCache().getAuthToken();
+        if(token != null){
+            Request request = new Request.Builder()
+                    .url(this.baseHandlerURL.toString() + "/invite/")
+                    .post(RequestBody.create(MediaType.parse(DEFAULT_CONTENT_TYPE), userId))
+                    .addHeader(authHeaderName, token)
+                    .addHeader("Accept",DEFAULT_CONTENT_TYPE)
+                    .build();
+            this.httpClient.newCall(request).enqueue(new DeserializerHttpResponseHandler<>(callback, String.class));
+        }else{
+            callback.onFailure(null, new IOException("Could not fetch auth token"));
+        }
+    }
+
+    @Override
+    public void getFriends(BaseFaroRequestCallback<List<MinUser>> callback) {
+        String token = TokenCache.getTokenCache().getAuthToken();
+        if(token != null){
+            Request request = new Request.Builder()
+                    .url(this.baseHandlerURL.toString() + "/invite/")
+                    .addHeader(authHeaderName, token)
+                    .addHeader("Accept",DEFAULT_CONTENT_TYPE)
+                    .build();
+            Type minUserList = new TypeToken<ArrayList<MinUser>>(){}.getType();
+            this.httpClient.newCall(request).enqueue(new DeserializerHttpResponseHandler<>(callback, minUserList));
+        }else{
+            callback.onFailure(null, new IOException("Could not fetch auth token"));
+        }
+    }
+
+    @Override
+    public void unFriend(BaseFaroRequestCallback<String> callback, String userId) {
+        String token = TokenCache.getTokenCache().getAuthToken();
+        if(token != null){
+            Request request = new Request.Builder()
+                    .url(this.baseHandlerURL.toString() + "/remove?userId=" + userId)
+                    .post(RequestBody.create(MediaType.parse(DEFAULT_CONTENT_TYPE), userId))
+                    .addHeader(authHeaderName, token)
+                    .addHeader("Accept",DEFAULT_CONTENT_TYPE)
+                    .build();
+            this.httpClient.newCall(request).enqueue(new DeserializerHttpResponseHandler<>(callback, String.class));
+        }else{
+            callback.onFailure(null, new IOException("Could not fetch auth token"));
+        }
+    }
 }
